6c5bc7b0edaed6aa0c599b49535e882a25000856
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-06-15 17:20:54 +0300

    Add Karma and Karma coverage reporting

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+// Karma configuration
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'browserify'],
+    files: [
+      'test/index.js'
+    ],
+    exclude: [ ],
+    preprocessors: {
+      'test/index.js': [ 'browserify' ]
+    },
+    browserify: {
+      debug: true,
+      transform: [
+        [ 'babelify', { plugins: [ 'istanbul' ] } ]
+      ]
+    },
+    reporters: ['mocha', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Firefox', 'PhantomJS'],
+    browserNoActivityTimeout: 30000,
+    singleRun: true,
+    concurrency: Infinity,
+
+    coverageReporter: {
+      reporters: [
+        {
+          type: 'lcovonly',
+          dir: 'coverage',
+          subdir: function (browser) {
+            return browser.toLowerCase().split(/[ /-]/)[0];
+          }
+        },
+        { type: 'text-summary' }
+      ]
+    }
+  });
+};
