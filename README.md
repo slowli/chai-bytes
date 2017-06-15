55ee25086bcb9ce842449dbf5f59e659badb8405
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-06-15 14:58:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chai Assertions for Byte Arrays Equality
+
+**chai-equal-bytes** extends [Chai][chai] with a `equalBytes` function,
+which can be used to test equality of byte arrays (i.e., `Uint8Array` instances).
+
+## Basic Usage
+
+```javascript
+const expect = require('chai')
+  .use(require('chai-equal-bytes'))
+  .expect();
+
+var buffer = new Uint8Array([ 1, 2, 3, 4, 5 ]);
+expect(buffer).to.equalBytes('0102030405');
+```
+
+An expected value passed to `equalBytes` can be:
+
+- Hexadecimal string, e.g., `c0ffee`
+- Array, e.g., `[1, 2, 3]`
+- Array-like object (i.e., an object having the `length` property
+  and items accessible by integer properties).
+  This includes `Uint8Array` instances
+
+If the passed value does not fall into any of these categories,
+a `TypeError` is thrown.
+
+## License
+
+(c) 2017 Alex Ostrovski
+
+**chai-equal-bytes** is available under [Apache-2.0 license](LICENSE).
+
+[chai]: https://chaijs.com/
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const every = function (buffer, predicate) {
+  if (Uint8Array && Uint8Array.prototype.every) {
+    return buffer.every(predicate);
+  } else {
+    for (var i = 0; i < buffer.length; i++) {
+      if (!predicate(buffer[i], i, buffer)) {
+        return false;
+      }
+      return true;
+    }
+  }
+};
+
+/**
+ * A simple plugin for Chai that compares byte arrays on per-element basis.
+ *
+ * @example
+ *   expect(new Uint8Array([1, 2])).to.equalBytes([1, 2]);
+ *   expect(new Uint8Array[65, 66, 67])).to.equalBytes('414243');
+ */
+module.exports = function (chai, utils) {
+  const Assertion = chai.Assertion;
+
+  Assertion.addMethod('equalBytes', function (expected) {
+    if (typeof expected === 'string') {
+      if (expected.length % 2 !== 0 || !/^[0-9a-f]*$/i.test(expected)) {
+        throw new TypeError('Invalid hex string: ' + expected);
+      }
+    } else if (expected.length === undefined) {
+      throw new TypeError('equalBytes consumes string, array, or array-like object; got none of those');
+    }
+
+    var actual = this._obj;
+    new Assertion(actual).to.be.a('uint8array');
+
+    var assert = false;
+    if (typeof expected === 'string') {
+      // expected value is a hex string
+      assert = expected.length === actual.length * 2 &&
+        every(actual, (x, i) => x === parseInt(expected.substring(2 * i, 2 * i + 2), 16));
+    } else if (expected.length !== undefined) {
+      // expected value is an array
+      assert = expected.length === actual.length &&
+        every(actual, (x, i) => expected[i] === x);
+    }
+
+    this.assert(
+      assert,
+      'expected #{this} to equal #{exp}',
+      'expected #{this} to not equal #{exp}',
+      expected
+    );
+  });
+};
